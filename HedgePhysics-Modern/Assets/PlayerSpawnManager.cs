5cee5492b4c6c220670f67263510a83055bc068c
KnucklesTheEchidna2399 <sajidur78@gmail.com>
2017-08-18 15:12:20 +0600

    things

---
+++ HedgePhysics-Modern/Assets/PlayerSpawnManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawnManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (FindObjectOfType<PlayerBhysics>()) {
+            FindObjectOfType<PlayerBhysics>().transform.position = transform.position;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
